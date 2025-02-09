34246c0a1f73d952b4d69b9ee3143cac08ea6395
gag <gagharut@gmail.com>
2025-02-09 22:59:26 +0300

    update

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -18,6 +18,7 @@ export const Video = () => {
                 <div className="video_content_subtitle outfit">Wedding Day</div>
                 <div className="video_content_paragraph ">[ so this is love ]</div>
                 <div className="video_content_date outfit">28 / 04 / 25</div>
+                <span className="loader_down"></span>
             </div>
         </div>
     );

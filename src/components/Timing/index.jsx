6db62804261d620d8891987546c1ade7921d2fa4
gag <gagharut@gmail.com>
2025-02-11 11:17:44 +0300

    update

--- src/components/Timing/index.jsx
+++ src/components/Timing/index.jsx
@@ -3,23 +3,37 @@ import './index.scss';
 export const Timing = () => {
     return (
         <div className="timing">
-            <div className="timing_title">ТАЙМИНГ</div>
-            <ul className="timing_list">
+            <div className="timing_title outfit">ТАЙМИНГ</div>
+            <ul className="timing_list mont">
                 <li className="timing_list_item">
-                    <div className="timing_list_item-left">15:00</div>
-                    <div className="timing_list_item-right">Lorem ipsum dolor.</div>
+                    <div className="timing_list_item-left">12:00</div>
+                    <div className="timing_list_item-right1 outfit">
+                        <div className="timing_list_item-right">ВЕНЧАНИЕ В ЦЕРКВИ</div>
+                    </div>
                 </li>
                 <li className="timing_list_item">
-                    <div className="timing_list_item-left">15:00</div>
-                    <div className="timing_list_item-right">Lorem ipsum dolor sit.</div>
+                    <div className="timing_list_item-left">15:30</div>
+                    <div className="timing_list_item-right1 outfit">
+                        <div className="timing_list_item-right">СБОР ГОСТЕЙ</div>
+                    </div>
                 </li>
                 <li className="timing_list_item">
-                    <div className="timing_list_item-left">15:00</div>
-                    <div className="timing_list_item-right">Lorem ipsum dolor sit amet.</div>
+                    <div className="timing_list_item-left">16:00</div>
+                    <div className="timing_list_item-right1 outfit">
+                        <div className="timing_list_item-right">СВАДЕБНАЯ ЦЕРЕМОНИЯ</div>
+                    </div>
                 </li>
                 <li className="timing_list_item">
-                    <div className="timing_list_item-left">15:00</div>
-                    <div className="timing_list_item-right">Lorem ipsum dolor.</div>
+                    <div className="timing_list_item-left mont">17:00</div>
+                    <div className="timing_list_item-right1 outfit">
+                        <div className="timing_list_item-right">ПРАЗДНИЧНЫЙ УЖИН</div>
+                    </div>
+                </li>
+                <li className="timing_list_item">
+                    <div className="timing_list_item-left h-23">23:00</div>
+                    <div className="timing_list_item-right1 outfit">
+                        <div className="timing_list_item-right">ЗАВЕРШЕНИЕ ТОРЖЕСТВА</div>
+                    </div>
                 </li>
             </ul>
         </div>

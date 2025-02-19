d378dfc8dc620d969ecb546bdd8bb3704cd13265
gag <gagharut@gmail.com>
2025-02-19 09:49:20 +0300

    fix

--- src/components/DressCode/index.jsx
+++ src/components/DressCode/index.jsx
@@ -11,20 +11,22 @@ export const DressCode = () => {
                 <div className="dress-code_title">
                     <span className="great">Дресс Код</span>
                 </div>
-                <div className="dress-code_description mont ">
-                    Нам будет особенно приятно, <br />
-                    если вы поддержите стиль <br />
-                    нашей свадьбы в своих образах
+                <div className="center">
+                    <div className="dress-code_description mont ">
+                        Нам будет особенно приятно, <br />
+                        если вы поддержите стиль <br />
+                        нашей свадьбы в своих образах
+                    </div>
                 </div>
             </div>
             <div className="dress-code_palette">
                 <Palette />
             </div>
-            <div className="dress-code_subtitle great">Девушки:</div>
+            <div className="dress-code_subtitle great">Девушки: Оливковый</div>
             <div className="dress-code_poster">
                 <Swipe />
             </div>
-            <div className="dress-code_subtitle great">Парни:</div>
+            <div className="dress-code_subtitle great">Парни: Серый</div>
             <div className="dress-code_poster">
                 <Swipe2 />
             </div>
@@ -71,9 +73,6 @@ const Swipe = () => {
             <SwiperSlide>
                 <img src="/5bf4a32591d7a2a101d62b9eff493aa8.jpg" alt="welcome" />
             </SwiperSlide>
-            <SwiperSlide>
-                <img src="/2d0588128d9d6d559524adc06b554b58.jpg" alt="welcome" />
-            </SwiperSlide>
         </Swiper>
     );
 };
@@ -110,9 +109,6 @@ const Swipe2 = () => {
             </SwiperSlide>
             <SwiperSlide>
                 <img src="/2be7ed0c39165376f2b5063d5c487f2d.jpg" alt="welcome" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="/f986b52a16b4311e462e2213944c4e29.jpg" alt="welcome" />
             </SwiperSlide>
             <SwiperSlide>
                 <img src="/b1fef8dfed03a9396c597020f9ee3004.jpg" alt="welcome" />
